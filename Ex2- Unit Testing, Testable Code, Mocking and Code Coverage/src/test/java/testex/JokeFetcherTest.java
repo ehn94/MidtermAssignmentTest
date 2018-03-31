b8b94d046719df06eb23409766d922c9b96865f2
Emilie <ehn1994@gmail.com>
2018-03-31 11:25:41 +0200

    First working test. To be refactored

--- Ex2- Unit Testing, Testable Code, Mocking and Code Coverage/src/test/java/testex/JokeFetcherTest.java
+++ Ex2- Unit Testing, Testable Code, Mocking and Code Coverage/src/test/java/testex/JokeFetcherTest.java
@@ -6,6 +6,7 @@
 package testex;
 
 import interfaces.IDateFormatter;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -16,6 +17,8 @@ import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*; 
+import static org.hamcrest.CoreMatchers.*; 
 
 /**
  *
@@ -28,77 +31,65 @@ public class JokeFetcherTest {
     @Mock
     IDateFormatter dateFormatter;
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
     }
 
     /**
      * Test of getAvailableTypes method, of class JokeFetcher.
      */
+  
+    
     @Test
     public void testGetAvailableTypes() {
         System.out.println("getAvailableTypes");
         JokeFetcher instance = new JokeFetcher(dateFormatter);
-        List<String> expResult = null;
+        List<String> expected = Arrays.asList("eduprog", "chucknorris", "moma", "tambal");
         List<String> result = instance.getAvailableTypes();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expected, result);
     }
 
     /**
      * Test of isStringValid method, of class JokeFetcher.
      */
-    @Test
-    public void testIsStringValid() {
-        System.out.println("isStringValid");
-        String jokeTokens = "";
-        JokeFetcher instance = new JokeFetcher(dateFormatter);
-        boolean expResult = false;
-        boolean result = instance.isStringValid(jokeTokens);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getJokes method, of class JokeFetcher.
-     */
-    @Test
-    public void testGetJokes() throws Exception {
-        System.out.println("getJokes");
-        String jokesToFetch = "";
-        String timeZone = "";
-        JokeFetcher instance = new JokeFetcher(dateFormatter);
-        Jokes expResult = null;
-        Jokes result = instance.getJokes(jokesToFetch, timeZone);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of main method, of class JokeFetcher.
-     */
-    @Test
-    public void testMain() throws Exception {
-        System.out.println("main");
-        String[] args = null;
-        JokeFetcher.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+//    @Test
+//    public void testIsStringValid() {
+//        System.out.println("isStringValid");
+//        String jokeTokens = "";
+//        JokeFetcher instance = new JokeFetcher(dateFormatter);
+//        boolean expResult = false;
+//        boolean result = instance.isStringValid(jokeTokens);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getJokes method, of class JokeFetcher.
+//     */
+//    @Test
+//    public void testGetJokes() throws Exception {
+//        System.out.println("getJokes");
+//        String jokesToFetch = "";
+//        String timeZone = "";
+//        JokeFetcher instance = new JokeFetcher(dateFormatter);
+//        Jokes expResult = null;
+//        Jokes result = instance.getJokes(jokesToFetch, timeZone);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of main method, of class JokeFetcher.
+//     */
+//    @Test
+//    public void testMain() throws Exception {
+//        System.out.println("main");
+//        String[] args = null;
+//        JokeFetcher.main(args);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
 }
